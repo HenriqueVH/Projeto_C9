6b47fa9fea28067f8bd0a2d2433ce7cf99cc4066
HenriqueVH <95575225+HenriqueVH@users.noreply.github.com>
2021-12-05 10:52:23 -0300

    Add files via upload
    PASTAS

---
+++ project_c9_pro_new-main/test_aray.js
@@ -0,0 +1,25 @@
+var media = [8.5, 9.0, 10, 6.8];
+
+
+var soma_das_notas = media[0] + media[1] + media[2] + media[3];
+
+
+ var media = soma_das_notas/media.length
+ console.log (media)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
